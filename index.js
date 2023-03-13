68423b44464bf7622f3a8ed5a9d4c189a94d4fc9
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-14 02:14:33 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+
+function findMatching(arr, exact) {
+return arr.filter(ele => ele.toLowerCase() == exact.toLowerCase());
+}
+
+function fuzzyMatch(arr, apprx) {
+	return arr.filter(ele => ele.startsWith(apprx));
+}
+
+function matchName(objs, exact) {
+	return objs.filter(ele => ele.name == exact);
+}
